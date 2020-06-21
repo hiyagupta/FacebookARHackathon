23647e8811a5216ff96817e678147f4060d272cd
Christopher Arnold <ChristopherArnold@namitentou.com>
2020-06-20 21:19:06 -0700

    Restructured the whole file, made a player update, tilting it will move the player now

--- SkateboarderPro/scripts/Obstacle.js
+++ SkateboarderPro/scripts/Obstacle.js
@@ -1,103 +1,63 @@
-/**
- * (c) Facebook, Inc. and its affiliates. Confidential and proprietary.
- */
-
-//==============================================================================
-// Welcome to scripting in Spark AR Studio! Helpful links:
-//
-// Scripting Basics - https://fb.me/spark-scripting-basics
-// Reactive Programming - https://fb.me/spark-reactive-programming
-// Scripting Object Reference - https://fb.me/spark-scripting-reference
-// Changelogs - https://fb.me/spark-changelog
-//
-// For projects created with v87 onwards, JavaScript is always executed in strict mode.
-//==============================================================================
-
-// How to load in modules
 const Scene = require('Scene');
 const Time = require('Time');
 const Patches = require('Patches');
+const DeviceMotion = require('DeviceMotion');
+export const Diagnostics = require('Diagnostics');
 
 //In milliseconds
 const calculationSpeed = 50;
 
-// Use export keyword to make a symbol available in scripting debug console
-export const Diagnostics = require('Diagnostics');
-
-//Adding obstacleUpdate to the timer so it'll update over time;
+//These are so the checks run over time
 const intervalTimer = Time.setInterval(obstacleUpdate, calculationSpeed);
+const playerTimer = Time.setInterval(playerUpdate, calculationSpeed);
 
+var playerMinClamp = -0.20;
+var playerMaxClamp = 0.20;
 var obstacleSpeed = 0.005;
 var previousValue = -0.15;
 var minX = -.1;
 var maxX = .1;
 
-var dead = Patches.getBooleanValue("dead").lastValue;
+var dead = false;
 
 function obstacleUpdate() {
-    Diagnostics.log(dead);
-    if (dead == true) return;
+    if (dead == false) {
+        Promise.all([Scene.root.findFirst('Obstacle'),]).then(function (results) {
+            const obstacle = results[0];
+            previousValue += obstacleSpeed;
 
-    //Example code for retrieving an object
-    Promise.all([Scene.root.findFirst('Obstacle'),]).then(function (results) {
-        const obstacle = results[0];
-        previousValue += obstacleSpeed;
+            if (previousValue > 0.15) {
+                obstacle.transform.x = GetRandomFloat(minX, maxX);
+                previousValue = -0.15;
+            }
 
-        if (previousValue > 0.15) {
-            obstacle.transform.x = getRandomFloat(minX, maxX);
-            previousValue = -0.15;
-        }
+            obstacle.transform.y = 0;
+            obstacle.transform.z = previousValue;
+            Patches.inputs.setPoint("obstaclePosition", obstacle.transform.position);
+        })
+    }
+}
 
-        obstacle.transform.y = 0;
-        obstacle.transform.z = previousValue;
-        Patches.setPointValue("obstaclePosition", obstacle.transform.position);
+function playerUpdate() {
+    //What the player does when it's not dead
+    if (dead == false) {
+        Promise.all([Scene.root.findFirst('Player')]).then(function (results) {
+            const player = results[0];
+            const playerTransform = player.transform;
+            const deviceWorldTransform = DeviceMotion.worldTransform;
+
+            //Best way to modify the ScalarSignal is via their methods because it's not treated like a number
+            playerTransform.x = deviceWorldTransform.rotationZ.clamp(playerMinClamp, playerMaxClamp).neg().div(2);
+            Patches.inputs.setPoint("playerPosition", player.transform.position);
+        })
+    }
+
+    //If alive check for player
+    Patches.outputs.getBoolean("dead").then(function (results) {
+        dead = results.pinLastValue();
     })
 }
 
-function getRandomFloat(min, max) {
+function GetRandomFloat(min, max) {
     return Math.random() * (max - min) + min;
 }
-
-// To use variables and functions across files, use export/import keyword
-// export const animationDuration = 10;
-
-// Use import keyword to import a symbol from another file
-// import { animationDuration } from './script.js'
-
-// To access scene objects
-// const directionalLight = Scene.root.find('directionalLight0');
-
-// To access class properties
-// const directionalLightIntensity = directionalLight.intensity;
-
-// To log messages to the console
-// Diagnostics.log('Console message logged from the script.');
-
-//////////////////////////////////////////////////////////////////////////////////////
-//adding DeviceMotionController code from documentation
-
-//==============================================================================
-// The following example demonstrates how to bind device rotation to an object.
-//
-// Project setup:
-// - Insert a plane
-// - Make sure the Device Motion capability is added in the properties
-//==============================================================================
-
-// Load in the required modules
-const DeviceMotion = require('DeviceMotion');
-
-// Locate the plane in the Scene
-const player = Scene.root.find('Player');
-
-Patches.setPointValue("playerPosition", player.transform.position);
-
-// Store a reference to the transform of the plane and the world transform of
-// the DeviceMotion module
-const planeTransform = player.transform;
-const deviceWorldTransform = DeviceMotion.worldTransform;
-
-// Bind the rotation of the device to the plane
-planeTransform.rotationX = deviceWorldTransform.rotationX;
-planeTransform.rotationY = deviceWorldTransform.rotationY;
-planeTransform.rotationZ = deviceWorldTransform.rotationZ;

59150c42755ca0e9c699f4ca378c93cb28b15e4f
Christopher Arnold <ChristopherArnold@namitentou.com>
2020-06-24 01:46:22 -0700

    Swapped out the skateboard. THREW OUT ALL MY CODE CAUSE THERE WAS SOME MEMORY LEAK. Collision works better now. Everything is pretty much a patch now

---
+++ SkateboarderPro/scripts/script.js
@@ -0,0 +1,48 @@
+const Scene = require('Scene');
+const Patches = require('Patches');
+export const Diagnostics = require('Diagnostics');
+const Time = require('Time');
+
+var jumping = false;
+
+var playerTransform;
+var obstacleTransform;
+
+Promise.all([Scene.root.findFirst('Player')]).then(function (results) {
+    const player = results[0];
+    playerTransform = player.transform;
+})
+
+Promise.all([Scene.root.findFirst('Obstacle')]).then(function (results) {
+    const obstacle = results[0];
+    obstacleTransform = obstacle.transform;
+})
+
+Time.setInterval(jumpingUpdate, 50);
+
+function jumpingUpdate() {
+    if (playerTransform.y.pinLastValue() != 0) {
+        jumping = true;
+    }
+    else {
+        jumping = false;
+    }
+
+    Patches.inputs.setBoolean("jumping", jumping);
+    Patches.inputs.setBoolean("collided", collisionCheck());
+}
+
+function collisionCheck() {
+    var collided = false;
+
+    //The skateboarder is jumping, pulling off some sick crazy tubular moves
+    if (playerTransform.y.pinLastValue() < 0.010) {
+        var a = playerTransform.x.pinLastValue() - obstacleTransform.x.pinLastValue();
+        var b = playerTransform.z.pinLastValue() - obstacleTransform.z.pinLastValue();
+
+        var c = Math.sqrt(a * a + b * b);
+        if (c < 0.15) collided = true;
+    }
+
+    return collided;
+}
